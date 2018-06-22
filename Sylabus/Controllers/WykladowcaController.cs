9c0943484ffbea19938ff0e81d70f5ed6eda1eaa
wojjar2 <wojjar2@st.amu.edu.pl>
2018-06-22 10:01:41 +0200

    Dodanie menu Administracyjnego

---
+++ Sylabus/Controllers/WykladowcaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sylabus.Controllers
+{
+    public class WykladowcaController : Controller
+    {
+        // GET: Wykladowca
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
